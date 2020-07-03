c72eb98a498e482f355536424f1a4797b859ade5
Small Luo <63465766+Sam-Small-Luo@users.noreply.github.com>
2020-07-03 14:06:35 +0800

    Delete getimage_bing.py

--- P/getimage_bing.py
+++
@@ -1,7 +0,0 @@
-from google_images_download_master import bing_scraper
-
-def crawler_bing(key):
-    response=bing_scraper.googleimagesdownload()  
-    arguments = {"url":"https://www.bing.com/images/search?q="+key,"download":True,"prefix":key,"limit":1,"print_urls":True,"chromedriver":".\google_images_download_master\chromedriver"} 
-    paths = response.download(arguments)
-    return paths
